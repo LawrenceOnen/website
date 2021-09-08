a38c83661add56c24f47b134bada18d6b33e793c
Lawrence Onen <lawrenceonen83@gmail.com>
2021-09-08 04:57:22 +0300

    updating about us

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -3,17 +3,16 @@ import React, { Component }  from 'react';
 export default function About() {
     return (
     <section id="about">
-    <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+    <div className="container mx-auto flex px-5 py-35 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          Hi, I'm Lawrence.
+          Technugget.
           <br className="hidden lg:inline-block" />I love to build amazing
           apps.
         </h1>
         <p className="mb-8 leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-          Laborum, voluptas natus?
+        We access, manage, and analyze data from different sources to design technological tools
+        needed for data-driven decision-making. By leveraging and operationalizing artificial intelligence, we adopt a human-centered approach to empower local and global communities based on their set of needs to better target, implement, and evaluate their programs and initiatives. We set up governance frameworks and manage communities to ensure a sustainable impact.
         </p>
         <div className="flex justify-center">
           <a
